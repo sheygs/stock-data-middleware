78cba92e446c3bae0c6cb15cdaa6663b7265986c
Olusegun <segun.ekoh@gmail.com>
2021-12-14 18:01:43 +0100

    chore(utils): Helper functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,10 +1,18 @@
 const ALLOWED_AGGREGATE_BARS_QUERY = ['limit', 'sort', 'adjusted'];
 
-const ALLOWED_GROUPED_DAILY_BARS_QUERY = ['limit', 'page', 'adjusted'];
+const ALLOWED_GROUPED_DAILY_BARS_QUERY = [
+        'limit',
+        'page',
+        'adjusted',
+        'cost',
+        'percentPer',
+        'name',
+        'gain',
+];
 
 const ALLOWED_DAILY_OPEN_CLOSE = ['adjusted'];
 
-const pagination = (results, page = 1, limit = 10) => {
+const paginate = (results, page = 1, limit = 10) => {
         const start = (page - 1) * limit;
         const end = page * limit;
         return results.slice(start, end);
@@ -31,12 +39,57 @@ const validateQueryValues = (reqQuery) => {
         return valuesArr.every((value) => Boolean(parseInt(value)));
 };
 
+const handleMap = (result) => {
+        return {
+                ...result,
+                g: +(result.c - result.o).toFixed(2),
+                p: +(((result.c - result.o) / result.o) * 100).toFixed(2),
+        };
+};
+
+const extractValueOperator = (queryValue) => {
+        if (typeof queryValue === 'object') {
+                if (queryValue['lte']) {
+                        return { value: queryValue['lte'], operator: 'lte' };
+                }
+                if (queryValue['gte']) {
+                        return { value: queryValue['gte'], operator: 'gte' };
+                }
+        } else {
+                return { value: queryValue, operator: 'eq' };
+        }
+};
+
+const filterCondition = (actualValue, param) => {
+        const { operator, value } = param;
+        switch (operator) {
+                case 'lte': {
+                        return actualValue <= value;
+                }
+
+                case 'gte': {
+                        return actualValue >= value;
+                }
+
+                case 'eql': {
+                        return actualValue === value;
+                }
+
+                default: {
+                        return false;
+                }
+        }
+};
+
 export {
-        pagination,
+        paginate,
         allowedQueries,
         ALLOWED_AGGREGATE_BARS_QUERY,
         ALLOWED_GROUPED_DAILY_BARS_QUERY,
         ALLOWED_DAILY_OPEN_CLOSE,
         clientQuery,
         validateQueryValues,
+        handleMap,
+        extractValueOperator,
+        filterCondition,
 };
