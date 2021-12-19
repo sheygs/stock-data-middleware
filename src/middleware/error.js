d03b2de792a4fc9c216852935f9a9634abd57f70
Olusegun <segun.ekoh@gmail.com>
2021-12-19 09:48:37 +0100

    Some modifications

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -4,13 +4,14 @@ import logger from '../utils/logger';
 
 const errorHandler = (error, req, res, next) => {
         logger.error(
-                `error:::${error}message:::${error.message}::status:::${error.status}::isOperational:::${error.isOperational}\n${error.stack}`
+                `Message:::${error.message}::Status:::${error.status}::isOperational:::${error.isOperational}\n${error.stack}`
         );
         const statusCode = checkStatusCode(error.message) || 500;
 
         logger.info(`statusCode: ${statusCode}`);
 
         sendErrorResponse(res, statusCode, error.message);
+
         next();
 };
 
